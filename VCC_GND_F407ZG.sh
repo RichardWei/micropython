d06d2833ebe26d387ab544293c73981f38c71de4
jerry <wz@test-pad.com>
2025-12-23 11:53:13 +0800

    stm32/ports add sram support for stm32f407

---
+++ VCC_GND_F407ZG.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# chmod +x py_env.sh
+BOARD=VCC_GND_F407ZG 
+make -C mpy-cross
+cd ports/stm32
+rm -rf build-${BOARD}
+# make BOARD=${BOARD} LTO=1 
+make BOARD=${BOARD} LTO=1
+FIRMWARE_DIR=boards/${BOARD}
+mkdir -p ${FIRMWARE_DIR}
+cp build-${BOARD}/firmware.hex ${FIRMWARE_DIR}
+# cp build-${BOARD}/firmware0.bin ${FIRMWARE_DIR}
+# cp build-${BOARD}/firmware1.bin ${FIRMWARE_DIR}
